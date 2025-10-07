f52f9710d1c486f31031b45218a6756e1db0eb90
mmugila1 <mmugila1@ford.com>
2025-10-07 15:19:16 +0530

    build fix

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6+dhYyADeVIGbvytT3700Kxdw3GBhvXqJyy2JuHugTI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
